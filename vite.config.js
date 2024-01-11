1e61c1ec71ae0a707604028a25ed0391772b5c3c
Aleksandar Maric <115831251+aleksa2315@users.noreply.github.com>
2024-01-11 14:56:35 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,6 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  server:{
-    port: 8078
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
